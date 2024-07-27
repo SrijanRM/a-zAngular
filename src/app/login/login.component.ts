d63674c864f3b5128fd0272cda4cb2af479df40e
SrijanRM <118186623+SrijanRM@users.noreply.github.com>
2024-07-27 20:31:08 +0530

    route service

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,19 +8,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  email:any;
-  password:any;
-  constructor() { }
+  email: any = '';
+  password: any = '';
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
-    
+
   }
 
   // 11:23
 
-  Login(){
-    if(this.email==="admin@gmail.com" && this.password==="123"){
-      alert("login sucess ")
+  Login() {
+    console.log("login ", this.email, " ", this.password)
+
+    if (this.email === "admin@gmail.com" && this.password === "123") {
+      this.router.navigate(['/rooms', 'add']);
+      // this.router.navigateByUrl('/rooms/add');
     }
   }
 
