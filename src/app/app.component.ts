ad72def7cea2d86e6e13be9719ea20c12dbc6bef
SrijanRM <118186623+SrijanRM@users.noreply.github.com>
2024-07-28 13:08:46 +0530

    routing event works
    adding loader using rxjs / pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, NavigationStart, Router } from '@angular/router';
+import { filter } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'hotelInventoryApp';
+
+  constructor(private router: Router) {
+
+  }
+
+
+  ngOnInit(): void {
+
+    // this.router.events.subscribe((event) => {
+    //   console.log(event);
+    // })
+
+    // pipe rxjs 
+
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationStart)
+    ).subscribe((event) => {
+      console.log('naviagatin started ', event);
+    });
+
+    this.router.events.pipe(
+      filter((event) => event instanceof NavigationEnd)
+    ).subscribe((event) => {
+      console.log('naviagatin Ended ', event);
+    });
+
+  }
 }
